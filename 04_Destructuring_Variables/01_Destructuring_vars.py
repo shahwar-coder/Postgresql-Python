650de836d04aebfa565fb8028646721d135b8a51
shahwar-coder <naqvishahwar120@gmail.com>
2026-04-07 10:49:51 +0530

    destructuring vars in python

---
+++ 04_Destructuring_Variables/01_Destructuring_vars.py
@@ -0,0 +1,8 @@
+# Tuple
+t = 5, 11
+
+# Destructuring (unpacking)
+x, y = t
+
+# Print values
+print(x, y) # 5 11
---
+++ 04_Destructuring_Variables/02_Destructuring_with_*.py
@@ -0,0 +1,12 @@
+# List
+numbers = [1, 2, 3, 4, 5]
+
+# Unpacking
+head, *tail = numbers
+
+# Print results
+print(head)
+print(tail)
+
+# 1
+# [2, 3, 4, 5]
