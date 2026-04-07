55aca4b3edaf05918e906aeccbe109a5f66e7215
shahwar-coder <naqvishahwar120@gmail.com>
2026-04-07 10:14:29 +0530

    intersectio->common

---
+++ 01_Advanced_Set_Operations/03_Common_Friends.py
@@ -0,0 +1,13 @@
+"""
+Intersection - (Common ones)
+"""
+
+# Define sets
+art = {"Bob", "Jen", "Rolf", "Charlie"}
+science = {"Bob", "Jen", "Adam", "Anne"}
+
+# Find common elements
+both = art.intersection(science)
+
+# Print result
+print(both) # {'Bob', 'Jen'}
