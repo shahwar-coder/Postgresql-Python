e25dddcf1043d9ec32c095a82c4117e65b1a26b4
shahwar-coder <naqvishahwar120@gmail.com>
2026-04-07 11:38:50 +0530

    funcs agrument vs parameters

---
+++ 05_Function_Argument_Parameter/01_Function_Argument_vs_Parameter.py
@@ -0,0 +1,16 @@
+'''
+In Python, parameters and arguments refer to different concepts that are crucial in the context of functions.
+
+Parameters:
+Parameters are the variables that are defined in the function declaration.
+They act as placeholders for the values that will be passed to the function. 
+For example, in the function definition def add(x, y):, x and y are parameters.
+
+Arguments:
+Arguments are the actual values that you provide to the function when calling it. 
+For instance, in the function call add(5, 3), the values 5 and 3 are the arguments passed to the add function, 
+with 5 becoming the value of x and 3 becoming the value of y.
+In summary, parameters are used in function definitions to define what data the function needs, 
+while arguments are the actual data you provide when you call the function. 
+Each argument corresponds to a parameter in the function.
+'''
