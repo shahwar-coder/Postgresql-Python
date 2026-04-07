fbe6a25ff6e22fdb303e8c9ba3b25d2c87d27039
shahwar-coder <naqvishahwar120@gmail.com>
2026-04-07 10:24:50 +0530

    traversing dicts

---
+++ 03_Traversing_Dictionary/01_Traverse_dict.py
@@ -0,0 +1,11 @@
+# Dictionary of student attendance
+student_attendance = {"Rolf": 96, "Bob": 80, "Anne": 100}
+
+# Loop through dictionary
+for student, attendance in student_attendance.items():
+    print(f"{student}: {attendance}")
+
+# Output:
+# Rolf: 96
+# Bob: 80
+# Anne: 100
